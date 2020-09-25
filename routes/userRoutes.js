b123369387274502c272bd3085d9f7496ab41be9
renewss <2ibragimov01@gmail.com>
2020-09-25 20:49:50 +0500

    add lead routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,9 +12,16 @@ router.use(authController.protect);
 // Protected My Routes
 router.route('/me').get(userController.getMe);
 
+// Lead Routes
+router.route('/my').get(authController.restrictTo('Lead'), userController.getMy);
+
 // Admin Routes
 router.use(authController.restrictTo('Admin'));
-router.route('/').get(userController.getAll).post(userController.create);
+router
+  .route('/')
+  .get(userController.getAll)
+  .post(userController.create)
+  .delete(userController.deleteUsers);
 router
   .route('/:id')
   .get(userController.getOne)
